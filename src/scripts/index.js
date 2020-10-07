4ab47c5db99460ef39698e3ba7e4aa233ebb6300
Luan Fellipe <luan@vistasoft.com.br>
2020-10-06 22:29:41 -0300

    chore: add AOS animate library

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,12 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import '../styles/index.scss';
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
+window.addEventListener('load', () => {
+  AOS.init();
+});
