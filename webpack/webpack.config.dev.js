6ddc750a8876b1021418332f22ad90e33df08d48
Luan Fellipe <luan@vistasoft.com.br>
2020-10-06 23:15:16 -0300

    refactor: format inwebpack config file

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -9,19 +9,19 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-eval-source-map',
   output: {
-    chunkFilename: 'js/[name].chunk.js'
+    chunkFilename: 'js/[name].chunk.js',
   },
   devServer: {
     inline: true,
-    hot: true
+    hot: true,
   },
   plugins: [
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env.NODE_ENV': JSON.stringify('development'),
     }),
     new StylelintPlugin({
-      files: Path.join('src', '**/*.s?(a|c)ss')
-    })
+      files: Path.join('src', '**/*.s?(a|c)ss'),
+    }),
   ],
   module: {
     rules: [
@@ -31,8 +31,8 @@ module.exports = merge(common, {
         enforce: 'pre',
         loader: 'eslint-loader',
         options: {
-          emitWarning: true
-        }
+          emitWarning: true,
+        },
       },
       {
         test: /\.html$/i,
@@ -41,12 +41,12 @@ module.exports = merge(common, {
       {
         test: /\.js$/,
         include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader?sourceMap=true', 'postcss-loader', 'sass-loader']
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader?sourceMap=true', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
 });
