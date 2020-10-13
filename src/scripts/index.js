d5f523ad02911ec4f7194f243bae5f3c76e9d18f
Luan Fellipe <luan@vistasoft.com.br>
2020-10-12 22:16:57 -0300

    feat: products section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,8 @@
 import AOS from 'aos';
 
+import ProductsController from './controller/ProductsController';
+import ProductsComponent from './ui/components/ProductsComponent';
+
 import 'reset-css';
 import 'aos/dist/aos.css';
 import 'bootstrap-4-grid';
@@ -10,6 +13,12 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-window.addEventListener('load', () => {
+const productsController = new ProductsController();
+const productsComponent = new ProductsComponent('[data-js="productsComponent"]');
+
+window.onload = async function onload() {
   AOS.init();
-});
+
+  const products = await productsController.getProducts();
+  productsComponent.render(products);
+};
---
+++ src/scripts/ui/components/ProductsComponent.js
@@ -0,0 +1,45 @@
+export default class ProductsComponent {
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+  }
+
+  template(products) {
+    return (`
+      <div class="products">
+        ${products.map((product) => (`
+          <div class="product">
+            <img class="product__image" src="${product.image}" alt="imagem do produto" />
+            <div class="product__content">
+              <h3 class="product__name">${product.name}</h3>
+              <ul>
+                <li class="product__description">
+                  ${product.description}
+                </li>
+                <li class="product__old-price">
+                  De: R$${product.oldPrice}
+                </li>
+                <li class="product__price">
+                  Por: R$${product.price}
+                </li>
+                <li class="product__installments">
+                  ou ${product.installments.count}x de R$${product.installments.value}
+                </li>
+              </ul>
+              <button type="button" class="button button--primary button--block">Comprar</button>
+            </div>
+          </div>
+        `)).join('')}
+      </div>
+      <button 
+      class="button button--primary button--lg button--center" 
+      data-js="fetch-products"
+    >
+      Ainda mais produtos aqui!
+    </button>
+    `);
+  }
+
+  render(products) {
+    this.element.innerHTML = this.template(products);
+  }
+}
